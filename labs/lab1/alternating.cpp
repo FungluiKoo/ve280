117b2e1c636555b5ea9de763e0ee23d55f35660d
FungluiKoo <40118880+FungluiKoo@users.noreply.github.com>
2019-05-31 13:40:35 +0800

    Uploaded the codes during Lab

--- labs/lab1/alternating.cpp
+++ labs/lab1/alternating.cpp
@@ -4,12 +4,20 @@
 using namespace std;
 
 bool is_alternating(int num, int base) {
-    // TODO: Your implementation here
-    return false;
+    int n=num;
+    int num2=0; //to be the inversed-digit number
+    while(n>0){
+        num2= num2*base + n%base; //gradually, the least significant bit will become the most significant
+        n/=base;
+    }
+    if(num2==num){return true;} //yes
+    else{return false;} //no
 }
 
 void test_alternating() {
     assert(!is_alternating(24, 10));  // 24 is not an alternating number in base 10
-    // TODO: Add more test cases
+    assert(is_alternating(645, 2));
+    assert(!is_alternating(24, 8));
+    assert(!is_alternating(24, 16));
     cout << "Alternating number tests passed!" << endl;
 }
--- labs/lab1/duffinian.cpp
+++ labs/lab1/duffinian.cpp
@@ -3,13 +3,32 @@
 
 using namespace std;
 
+int GCD(int num1, int num2){
+    int numSmall=(num1<num2)?num1:num2;
+    int numBig=(num1<num2)?num2:num1;
+    int numTemp=numBig%numSmall;
+    while(numTemp!=0){
+        numBig=numSmall;
+        numSmall=numTemp;
+        numTemp=numBig%numSmall;
+    }
+    return numSmall;
+}
+
 bool is_duffinian(int num) {
-    // TODO: Your implementation here
-    return false;
+    int n=num;
+    int divisorSum=0;
+    for(int i=1; i<=num; i++){ 
+        if(n%i==0){divisorSum+=i;}
+    }
+    if(1==GCD(divisorSum,num)){return true;}
+    else{return false;}
 }
 
 void test_duffinian() {
     assert(!is_duffinian(6));  // 6 is not a Duffinian number
-    // TODO: Add more test cases
+    assert(is_duffinian(35));
+    assert(!is_duffinian(12));
+    assert(!is_duffinian(18));
     cout << "Duffinian number tests passed!" << endl;
 }
